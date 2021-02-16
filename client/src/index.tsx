e77e67718e6cf9198ec1ece4f3a259b7d9f213bb
OguraDaiki <8hachibee125@gmail.com>
2021-02-16 16:20:47 +0900

    react hook supports translation without props relay

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {TranslationProvider} from './Translate/hook';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TranslationProvider>
+      <App />
+    </TranslationProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
