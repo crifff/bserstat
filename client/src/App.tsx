ea2edf71d2d4f4df7788120ca8c97776ae9446ab
crifff <hoshina85@gmail.com>
2021-02-18 20:03:29 +0900

    about

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,6 +7,7 @@ import Armor from "./Components/Armor";
 import { Col, Layout, Menu, Radio, RadioChangeEvent, Row } from 'antd';
 import { BrowserRouter as Router, Link, Redirect, Route, Switch, useHistory, useParams } from "react-router-dom";
 import { useTranslate } from './Translate/hook';
+import About from "./Components/About";
 
 const {Content} = Layout;
 
@@ -65,7 +66,8 @@ function App() {
         <Content>
           <Switch>
             <Route exact path="/">
-              <Redirect to="/character/all"/>
+              <Header/>
+              <About />
             </Route>
             <Route path="/:category(character)/:tier(all|high)">
               <Header/>
@@ -104,8 +106,12 @@ const Header: FC = (prop): any => {
   }
 
   return <Row wrap={false} align={"middle"} className={"menu-row"}>
+    <Col style={{paddingLeft:"1rem"}}>
+      <Link to="/" >
+        <img src="/images/logo.png" alt="BS:ER Stat" height={32}/>
+      </Link>
+    </Col>
     <Col flex="none">
-
       <Menu selectedKeys={[`${category}_${tier}`]} mode="horizontal">
         <Menu.Item key="character_all">
           <Link to="/character/all">
@@ -139,7 +145,7 @@ const Header: FC = (prop): any => {
         </Menu.Item>
       </Menu>
     </Col>
-    <Col flex="auto" style={{textAlign: "right"}}>
+    <Col flex="auto" style={{textAlign: "right",paddingRight:"1rem"}}>
       <Radio.Group value={lang} onChange={handleLangChange}>
         <Radio.Button value="ja">ja</Radio.Button>
         <Radio.Button value="en">en</Radio.Button>
---
+++ client/src/Components/About.tsx
@@ -0,0 +1,35 @@
+import { FC } from "react";
+import { Col, Row, Typography } from "antd";
+
+const {Title, Paragraph, Text, Link} = Typography;
+const About: FC = (prop) => {
+  return <Row>
+    <Col span={24}>
+      <Typography style={{padding:"2rem"}}>
+        <Title>BS:ER Statについて</Title>
+        <Paragraph>
+          BS:ER statはBS:ER公式サイト(<a href={"https://playeternalreturn.com/ja/stats/"}>https://playeternalreturn.com/ja/stats/</a>)で提供されている統計情報をより便利に閲覧できるビューワーです。
+        </Paragraph>
+
+        <Title level={2}>データについて</Title>
+        <Paragraph>
+          <ul>
+            <li>
+              統計情報は公式の英語版を元に、前回集計時の統計情報との差分を表示しています。
+            </li>
+            <li>
+              公式のデータが更新されてからBS:ER statに反映されるまでに時間差があります。
+            </li>
+          </ul>
+        </Paragraph>
+
+        <Title level={2}>開発について</Title>
+        <Paragraph>
+          開発はgithub(<a href="https://github.com/crifff/bserstat">https://github.com/crifff/bserstat</a>)上で行われています。
+        </Paragraph>
+      </Typography>
+    </Col>
+  </Row>
+}
+
+export default About;
